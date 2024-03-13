f6b5ed739270c7a096d6dd54ee64de44b39fea43
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-13 19:21:03 -0300

    While e Break

---
+++ AulasUdemy/Estruturas de Repetição/whileAndBreak.py
@@ -0,0 +1,14 @@
+# condicao = True
+# while condicao:
+#     nome = input('Digite seu nome: ')
+#     print(f'Seu nome é {nome}')
+#     if nome == 'Sair':
+#         break
+# print('Acabou')
+
+contador = 0
+
+while contador < 10:
+    contador += 1
+    print(contador)
+print('Acabou')
