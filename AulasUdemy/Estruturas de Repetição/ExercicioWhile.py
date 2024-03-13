29f41f100dd14f3346567668db9856113d1cb087
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-13 19:26:53 -0300

    Exercício While

---
+++ AulasUdemy/Estruturas de Repetição/ExercicioWhile.py
@@ -0,0 +1,9 @@
+name = input('Digite seu Nome: ')
+name_size = len(name)
+new_string = ''
+index = 0
+while index < name_size:
+    new_string += '*'
+    new_string += name[index]
+    index += 1
+print(new_string)
