41462e5c81632ae42156ae7bedb1df37f8096da0
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-13 22:51:04 -0300

    Correção de alguns erros e modificação da lógica

--- AulasUdemy/Estruturas de Repetição/ExercicioCalculadora.py
+++ AulasUdemy/Estruturas de Repetição/ExercicioCalculadora.py
@@ -1,35 +1,39 @@
-pNumero = input('Digite o Primeiro Número: ')
-sNumero = input('Digite o Segundo Número: ')
-resultado = 0.0
-
-print('Escolha o Tipo de Operação')
-escolha = input('1 - Soma \n2 - Subtração \n3 - Multiplicação\n4 - Divisão \n')
-if pNumero.isdigit and sNumero.isdigit:
-    soma = float(pNumero) + float(sNumero)
-    sub = float(pNumero) - float(sNumero)
-    mult = float(pNumero) * float(sNumero)
-    div = float(pNumero) / float(sNumero)
-    if escolha.isdigit == True:
-        if int(escolha) == 1:
-            print(soma)
-        elif int(escolha) == 2:
-            print(sub)
-        elif int(escolha) == 3:
-            print(mult)
-        elif int(escolha) == 4:
-            print(div)
-        else:
-            print('O valor que você digitou na escolha não é válido')
+continuar = True
+while continuar:
+    pNumero = input('Digite o Primeiro Número: ')
+    sNumero = input('Digite o Segundo Número: ')
+    num_pNumero = 0
+    num_sNumero = 0
+    resultado = 0.0
+    nValidos = None
+    operadores = '+-*/'
+    print('Escolha o Tipo de Operação')
+    escolha = input('1 - Soma [+] \n2 - Subtração [-] \n3 - Multiplicação [*] \n4 - Divisão  [/] \n')
+    try:
+        num_pNumero = float(pNumero)
+        num_sNumero = float(sNumero)
+        nValidos = True
+    except:
+        nValidos = None
+    if nValidos is None:
+        print('Um ou ambos os Números Digitados são Inválidos')
+        continue
+    if escolha not in operadores:
+        print('Digite um Operador Válido')
+        continue
+    if len(escolha) > 1:
+        print('Escolha apenas um operador')
+        continue
+    if escolha == '+':
+        print(num_pNumero + num_sNumero)
+    elif escolha == '-':
+        print(num_pNumero - num_sNumero)
+    elif escolha == '*':
+        print(num_pNumero * num_sNumero)
     else:
-        if escolha == 'Soma': 
-            print(soma)
-        elif escolha == 'Subtração':
-            print(sub)
-        elif escolha == 'Multiplicação':
-            print(mult)
-        elif escolha == 'Divisão':
-            print(div)
-        else: 
-            print('O valor que você digitou na escolha não é válido')
-else:
-    print('Digite Apenas Números!!!')
+        print(num_pNumero / num_sNumero)    
+    
+    sair = input('Deseja sair? [s]im: ').lower().startswith('s')
+    if sair is True:
+        break
+print('Fim do Código')
