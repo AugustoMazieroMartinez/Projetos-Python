2e46de1885cfbc7f350e8059021d937f44552103
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-24 00:59:15 -0300

    Correção de um erro que aparecia ao tentar apagar qualquer valor

--- AulasUdemy/Estruturas de Repetição/ExercícioListaComprasArray.py
+++ AulasUdemy/Estruturas de Repetição/ExercícioListaComprasArray.py
@@ -11,19 +11,32 @@ while True:
         continue
     if entrada == 'a':
         if len(lista_compras) != 0:
-            apagar = input('Índice do valor a ser apagado: ')
-            lista_compras.remove(int(apagar))
-            os.system('cls')
-            print('O valor for removido!')
-            continue
+            try:
+                apagar = input('Índice do valor a ser apagado: ')
+                index = int(apagar)
+                del lista_compras[index]
+                os.system('cls')
+                print('O valor for removido!')
+                continue
+            except IndexError:
+                print('O índice não existe na lista.')
+                continue
+            except ValueError:
+                print('Por favor digite um número int.')
+                continue
+            except Exception:
+                print('Erro desconhecido.')
         else:
+            os.system('cls')
             print('Não há valores na lista para serem apagados!\n')
             continue
     if entrada == 'l':
         if len(lista_compras) != 0:
+            os.system('cls')
             for indice, valor in enumerate(lista_compras):
                 print(indice, valor)
             continue
         else:
+            os.system('cls')
             print('Nada para listar\n')
             continue
