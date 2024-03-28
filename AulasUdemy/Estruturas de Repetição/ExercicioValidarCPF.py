66f64757416e43233e93bc4b49a4e8fde5602362
Augusto Martinez <augusto.mmartinez@gmail.com>
2024-03-27 21:54:59 -0300

    Algumas mudanças nas formatações e orderm das variáveis

--- AulasUdemy/Estruturas de Repetição/ExercicioValidarCPF.py
+++ AulasUdemy/Estruturas de Repetição/ExercicioValidarCPF.py
@@ -1,15 +1,17 @@
 cpf = '74682489070'
 cpf_9dig = cpf[:9]
-cpf_10dig = cpf[:10]
 contador1 = 10
-contador2 = 11
 pDigito = 0
-sDigito = 0
 for n in cpf_9dig:
     pDigito += int(n) * contador1
     contador1 -= 1
 pDigito = (pDigito * 10) % 11
 pDigito= pDigito if pDigito <= 9 else 0
+
+
+cpf_10dig = f'{cpf_9dig}{pDigito}'
+sDigito = 0
+contador2 = 11
 for n in cpf_10dig:
     sDigito += int(n) * contador2
     contador2 -= 1
