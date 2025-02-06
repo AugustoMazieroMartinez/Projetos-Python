e6e823e7d220c319f791fad488c08149f27d49c3
Augusto Martinez <augusto.mmartinez@gmail.com>
2025-02-06 00:36:07 -0300

    Exercicio fatorial

---
+++ Projetos/teste.py
@@ -0,0 +1,24 @@
+"""
+Function to calculate the factorial of a given number.
+
+Args:
+    n (int): The number for which the factorial is to be calculated. Must be a non-negative integer.
+
+Returns:
+    int: The factorial of the given number.
+
+Raises:
+    ValueError: If the input is a negative integer.
+
+Example:
+    >>> factorial(5)
+    120
+"""
+value = int(input('Digite um número: '))
+def factorial(value):
+    if value < 0:
+        raise ValueError('O valor deve ser positivo')
+    if value == 0:
+        return 1   
+    return value * factorial(value - 1)
+print(factorial(value))
