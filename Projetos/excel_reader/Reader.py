876187b81b3eb43be744736cea79ffe4c2800882
Augusto Martinez <augusto.mmartinez@gmail.com>
2025-04-12 17:12:42 -0300

    começo de estudos com manipulação de excel usando python para um projeto futuro

---
+++ Projetos/excel_reader/Reader.py
@@ -0,0 +1,7 @@
+import openpyxl as px
+
+wb = px.load_workbook('videogamesales.xlsx')
+ws = wb.active
+ws = wb['vgsales']
+# print('Total number of rows: ' + str(ws.max_row) + "\nTotal number of columns:" + str(ws.max_column))  
+print('The value in cell A1 is: ' + str(ws['A1'].value))
