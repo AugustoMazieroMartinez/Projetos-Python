8970054848d41f3af9e02c38f86821b0f07ff0ee
Augusto Martinez <augusto.mmartinez@gmail.com>
2025-04-20 01:07:58 -0300

    bazinga

--- Projetos/excel_reader/Reader.py
+++ Projetos/excel_reader/Reader.py
@@ -3,5 +3,6 @@ import openpyxl as px
 wb = px.load_workbook('videogamesales.xlsx')
 ws = wb.active
 ws = wb['vgsales']
+
 # print('Total number of rows: ' + str(ws.max_row) + "\nTotal number of columns:" + str(ws.max_column))  
-print('The value in cell A1 is: ' + str(ws['A1'].value))
+print('The value in cell A1 is: ' + str(ws['c1'].value))
